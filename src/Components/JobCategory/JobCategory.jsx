8efa9597badd9957a1095113c85d81a30e7c817b
Abu Abdullah <abdullah72308@gmail.com>
2023-04-11 02:11:42 +0600

    home page mobile responsive

--- src/Components/JobCategory/JobCategory.jsx
+++ src/Components/JobCategory/JobCategory.jsx
@@ -15,13 +15,13 @@ const JobCategory = () => {
         <div className='job-category'>
             <h1>Job Category List</h1>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
-<div>
-{
+            <div>
+                {
                     lists.map(list => <JobCategoryList
                         list={list}
                     ></JobCategoryList>)
                 }
-</div>
+            </div>
 
         </div>
     );
